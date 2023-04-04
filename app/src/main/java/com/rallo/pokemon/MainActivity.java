e15bb0d11411869ef7149b36edf76aeb34d33c10
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-04-04 15:32:43 +0200

    feat: add Language select

--- app/src/main/java/com/rallo/pokemon/MainActivity.java
+++ app/src/main/java/com/rallo/pokemon/MainActivity.java
@@ -5,20 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements PostExecuteActivity<Pokemon> {
-    private final String TAG = "frallo "+getClass().getSimpleName();
+    public static String language = "English";
+    private final String TAG = "frallo " + getClass().getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.go).setOnClickListener( clic -> {
+        findViewById(R.id.go).setOnClickListener(clic -> {
             String url = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/17d33dc111ffcc12b016d6485152aa3b1939c214/pokedex.json";
-            new HttpAsyncGet<>(url, Pokemon.class, this, new ProgressDialog(clic.getContext()) );
+            new HttpAsyncGet<>(url, Pokemon.class, this, new ProgressDialog(clic.getContext()));
+        });
+
+        // language spinner listener
+        ((Spinner) findViewById(R.id.spinner)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                language = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                language = "English";
+            }
         });
     }
 
@@ -26,6 +44,6 @@ public class MainActivity extends AppCompatActivity implements PostExecuteActivi
     @Override
     public void onPostExecutePokemons(List<Pokemon> itemList) {
         Pokemon pokemonFirst = itemList.get(0);
-        Log.d(TAG,"First pokemon = " + pokemonFirst.getName());
+        Log.d(TAG, "First pokemon = " + pokemonFirst);
     }
 }
